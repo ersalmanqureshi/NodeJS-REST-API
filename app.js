8931164be3640cc7a2aa33afcdcbe5494b473b20
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-04-26 20:46:44 +0530

    Response as json from node, setup morgan

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
+
+app.use(morgan("combined"))
 
 //ping server on localhost:3003
 app.listen("3003", () => {
@@ -9,4 +12,12 @@ app.listen("3003", () => {
 app.get("/", (req, res) => {
     console.log("Responding to route");
     res.send("Hello from root")
+})
+
+app.get("/users", (req, res) => {
+    console.log("Nodemon")
+    var user1 = {firstName: "Salman", lastName: "Qureshi"}
+    const user2 = {firstName: "Sharukh", lastName: "Qureshi"}
+    res.json([user1, user2])
+    //res.send("Hello Nodemon")
 })
