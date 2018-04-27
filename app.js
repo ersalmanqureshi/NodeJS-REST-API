847154d65bb52872a092bc3380c3d39b4bf653be
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-04-28 01:11:41 +0530

    Refactor code

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express")
 const app = express()
 const morgan = require("morgan")
 const mysql = require("mysql")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+const router = require('./routes/user.js')
+
+app.use(router)
 
 app.use(morgan("short"))
 
@@ -12,72 +19,8 @@ app.listen("3003", () => {
     console.log("Server is up and listening 3003");
 })
 
-app.post("/user_create", (req, res) => {
-    console.log("Trying to create a new user");
-    res.end()
-})
 
 app.get("/", (req, res) => {
     console.log("Responding to route");
     res.send("Hello from root")
-})
-
-app.get("/users", (req, res) => {
-
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'isalman_mysql_db'
-    })
-
-    const queryString = "SELECT * FROM users"
-
-    connection.query(queryString, (err, results, fields) => {
-
-        if (err) {
-            console.log("Failed to search for user: ", err);
-            res.sendStatus(500)
-            return
-
-            //throw err
-        }
-
-        console.log("Fetch users successfully");
-
-        res.json(results)  
-    })
-})
-
-app.get("/user/:id", (req, res) => {
-    console.log("Fetching user with id:", req.params.id)
-
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'isalman_mysql_db'
-    })
-
-    const userId = req.params.id
-    const queryString = "SELECT * FROM users WHERE id = ?"
-
-    connection.query(queryString, [userId] , (err, results, fields) => {
-
-        if (err) {
-            console.log("Failed to search for user: ", err);
-            res.sendStatus(500)
-            return
-
-            //throw err
-        }
-
-        console.log("Fetch users successfully");
-
-        var users = results.map((result) => {
-            return { firstName: result.first_name, lastName: result.last_name}
-        })
-
-        res.json(users)  
-    })
-
-    //res.end()
 })
---
+++ routes/user.js
@@ -0,0 +1,93 @@
+const express = require("express")
+const mysql = require("mysql")
+const router = express.Router()
+
+function getConnection() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'isalman_mysql_db'
+    })
+}
+
+router.get("/messages", (req, res) => {
+    console.log("Showing messages");
+    res.end()
+})
+
+router.get("/users", (req, res) => {
+
+    const connection = getConnection()
+    const queryString = "SELECT * FROM users"
+
+    connection.query(queryString, (err, results, fields) => {
+
+        if (err) {
+            console.log("Failed to search for user: ", err);
+            res.sendStatus(500)
+            return
+            //throw err
+        }
+
+        console.log("Fetch users successfully");
+
+        res.json(results)  
+    })
+})
+
+router.post("/user_create", (req, res) => {
+    console.log("Create a new user");
+    
+    const connection = getConnection()
+    const queryString = "INSERT INTO users (first_name, last_name) VALUES (? , ?)"
+    const firstName = req.body.create_first_name
+    const lastName = req.body.create_last_name
+
+    console.log("FirstName: " + firstName);
+    console.log("LastName: " + lastName);
+
+    connection.query(queryString, [firstName, lastName], (err, results, fields) => {
+        if (err) {
+            console.log("Failed to create a new user: " + err);
+            res.sendStatus(500)
+            return
+        }
+
+        console.log("Inserted a new user with id: " + results.insertId);
+        
+        res.end()
+    })
+
+    //res.end()
+})
+
+router.get("/user/:id", (req, res) => {
+    console.log("Fetching user with id:", req.params.id)
+
+    const connection = getConnection()
+    const userId = req.params.id
+    const queryString = "SELECT * FROM users WHERE id = ?"
+
+    connection.query(queryString, [userId] , (err, results, fields) => {
+
+        if (err) {
+            console.log("Failed to search for user: ", err);
+            res.sendStatus(500)
+            return
+
+            //throw err
+        }
+
+        console.log("Fetch users successfully");
+
+        var users = results.map((result) => {
+            return { firstName: result.first_name, lastName: result.last_name}
+        })
+
+        res.json(users)  
+    })
+
+    //res.end()
+})
+
+module.exports = router
